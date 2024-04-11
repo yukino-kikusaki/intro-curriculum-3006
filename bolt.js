6fbc25fdc30f832fcad11bbcfdcb7bb67e1aa265
yukino-kikusaki <yukino.kikusaki@azone-japan.co.jp>
2024-04-11 12:35:40 +0900

    おみくじ

--- bolt.js
+++ bolt.js
@@ -14,4 +14,10 @@ app.message(/hello/i, ({message, say}) => {
   say('こんにちは！');
 });
 
+app.message('おみくじ',({message, say}) =>{
+  const lots = ['大吉','吉','中吉','末吉','凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot},<@${message.user}>`);
+});
+
 app.start();
